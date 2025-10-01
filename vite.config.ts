97b45005269a773c5c17f5971e6778d9c4c2b99b
shivanand k <kshivananda534@gmail.com>
2025-10-01 16:44:39 +0530

    path changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
-  base : '/LinkLeaf'
+  base : '/LinkLeaf',
+  build: {
+    chunkSizeWarningLimit: 1000,  
+  },
 })
